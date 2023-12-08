4cc78cf64da62e1cc2d609c25114598aad7ac021
muteenk <kundangarmuteen@gmail.com>
2023-12-08 21:35:36 +0530

    Navbar added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Navbar from '../Navbar'
+import Bubbles from '../Bubbles'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    
+      <Bubbles top='-36%' left='-9%' dimension='50em' color='rgba(255, 0, 138, 0.08)'/>
+      <Bubbles top='17%' left='65%' dimension='50em' color='rgba(0, 102, 255, 0.09)'/>
+      <Navbar/>
+    </>
   )
 }
 
