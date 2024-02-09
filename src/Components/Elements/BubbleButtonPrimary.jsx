d3e8a2ded9d61c648a9e01d2f2d6f6cc8e51377d
muteenk <kundangarmuteen@gmail.com>
2024-02-10 03:11:55 +0530

    Hero on Home made responsive

--- src/Components/Elements/BubbleButtonPrimary.jsx
+++ src/Components/Elements/BubbleButtonPrimary.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const BubbleButtonPrimary = (props) => {
   return (
     <div className={`${props.className} flex justify-center items-center`}>
-        <Link to={props.sendTo} className='flex justify-center items-center h-[100px] w-[100px] bg-accent-color border-[2px] border-accent-color text-[white] my-[15px] rounded-[50%] font-semibold text-[30px] transition-all duration-[0.5s] hover:bg-[transparent] hover:border-[white] hover:text-accent-color'><i className="fa-solid fa-arrow-right"></i></Link>
+        <Link to={props.sendTo} className='flex justify-center items-center h-[100px] w-[100px] max-[455px]:h-[80px] max-[455px]:w-[80px] bg-accent-color border-[2px] border-accent-color text-[white] my-[15px] rounded-[50%] font-semibold text-[30px] transition-all duration-[0.5s] hover:bg-[transparent] hover:border-[white] hover:text-accent-color'><i className="fa-solid fa-arrow-right"></i></Link>
         <span className='text-[15px] font-semibold relative right-[22px] uppercase'>{props.text}</span>
     </div>
   )
--- src/Components/Home/HomeHero.jsx
+++ src/Components/Home/HomeHero.jsx
@@ -7,19 +7,18 @@ import HeroImg from '../../assets/img/home/home_hero_vector_1.svg'
 
 const HomeHero = () => {
   return (
-    <div className='flex justify-center items-center hero h-[100%]'>
+    <div className='flex flex-wrap justify-center items-center hero h-[100%] mt-[2em]'>
 
-        <div className='flex flex-col justify-center items-start py-[30px] px-[5em]'>
+        <div className='flex flex-col justify-center items-start py-[30px] px-[5em] max-[675px]:items-center'>
           <p className='font-primary text-accent-color font-semibold text-[15px] tracking-[3px]'>Your Vision | Our Expertise</p>
-          <h1 className='w-[7em] font-secondary font-semibold text-[100px] underline leading-[1em]'>Building Brands, Crafting Success</h1>
-          <p className='w-[17em] my-[30px] font-secondary text-[#8A8A8A] text-[32px] text-gray-700 text-opacity-100'>We help you make your brand story more powerful</p>
+          <h1 className='w-[7em] font-secondary font-semibold text-[100px] max-[1357px]:text-[75px]  max-[1100px]:text-[90px] max-[675px]:text-[60px] max-[455px]:text-[40px] underline leading-[1em]'>Building Brands, Crafting Success</h1>
+          <p className='w-[17em] my-[30px] font-secondary text-[#8A8A8A] text-[32px] text-gray-700 text-opacity-100 max-[675px]:text-[20px] text-center'>We help you make your brand story more powerful</p>
           <BubbleButtonPrimary className='mx-auto' sendTo='/services' text='VIEW OUR SERVICES' />
         </div>
 
-        <div className='w-[60%]'>
+        <div className='w-[30em] max-[1357px]:w-[24em] max-[1100px]:w-[45em]'>
           <img src={HeroImg} alt="" />
         </div>
-
 
     </div>
   )
