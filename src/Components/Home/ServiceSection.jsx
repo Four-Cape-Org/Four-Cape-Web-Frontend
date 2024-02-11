7d02756cc5484d7c7d224c7a027f3ee7c2072214
muteenk <kundangarmuteen@gmail.com>
2024-02-11 22:56:17 +0530

    Services section made responsive >> home page

--- src/Components/Home/ServiceSection.jsx
+++ src/Components/Home/ServiceSection.jsx
@@ -8,20 +8,20 @@ import CardImg1 from '../../assets/img/general/serviceCards/service_card_1.jpeg'
 
 const ServiceSection = () => {
   return (
-    <div className='relative top-0'>
-        <OverlayText className='absolute top-0 left-10 z-[0] text-[20vw] max-[1250px]:text-[18vw] max-[550px]:left-3' text='Services' />
+    <div className='relative top-0 md:mt-[14em] mt-[6em]'>
+        <OverlayText className='absolute lg:top-0 md:top-[-69px] sm:top-0 top-9 left-10 z-[0] text-[20vw] max-[1250px]:text-[18vw] max-[550px]:left-3' text='Services' />
 
         <div className="relative pt-[7em] flex flex-wrap justify-center items-center">
 
             <div className="flex flex-wrap justify-center items-center">
 
-                <div className="flex flex-col justify-center items-start mx-[5em] my-[7em] w-[20%] h-[48em]">
+                <div className="flex flex-col justify-center items-start lg:mx-[3em] md:mx-[3em] lg:my-[7em] lg:w-[25%] md:w-[37%] sm:w-[60%] w-[90%] mx-auto lg:h-[48em] md:h-[30em]">
                     <p className='text-accent-color font-bold font-primary text-[20px] my-[10px]'>Our Services</p>
                     <h2 className='font-secondary text-[4em] drop-shadow-xl'>What do we do ?</h2>
-                    <BubbleButtonSecondary className='my-[32px]' sendTo='/services' text='More of our Services' />
+                    <BubbleButtonSecondary className='my-[32px]' sendTo='/services' text='More Services' />
                 </div>
 
-                <div className="flex flex-col justify-center items-center mx-[5em] my-[7em] w-[20%]">
+                <div className="flex flex-col justify-center items-center lg:mx-[2em] sm:mx-[3em] lg:my-[7em] lg:w-[25%] md:w-[37%] sm:w-[50%] w-[90%]">
                     <img src={CardImg1} className='h-[25em] w-[21em] border-[10px] border-[white] shadow-2xl' alt="" />
                     <div className="flex flex-col justify-center items-start w-[88%] mt-[40px]">
                         <h5 className='text-[2em] font-secondary font-semibold drop-shadow-xl'>Card Title</h5>
@@ -30,7 +30,7 @@ const ServiceSection = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col justify-center items-center pt-[7em] mx-[5em] my-[7em] w-[20%]">
+                <div className="flex flex-col justify-center items-center lg:pt-[7em] lg:mx-[2em] sm:mx-[3em] lg:my-[7em] lg:w-[25%] md:w-[37%] sm:w-[50%] w-[90%]">
                     <img src={CardImg1} className='h-[25em] w-[21em] border-[10px] border-[white] shadow-2xl' alt="" />
                     <div className="flex flex-col justify-center items-start w-[88%] mt-[40px]">
                         <h5 className='text-[2em] font-secondary font-semibold drop-shadow-xl'>Card Title</h5>
@@ -39,7 +39,7 @@ const ServiceSection = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col justify-center items-center mx-[5em] w-[20%]">
+                <div className="flex flex-col justify-center items-center lg:mx-[2em] sm:mx-[3em] lg:w-[25%] md:w-[37%] sm:w-[50%] w-[90%]">
                     <img src={CardImg1} className='h-[25em] w-[21em] border-[10px] border-[white] shadow-2xl' alt="" />
                     <div className="flex flex-col justify-center items-start w-[88%] mt-[40px]">
                         <h5 className='text-[2em] font-secondary font-semibold drop-shadow-xl'>Card Title</h5>
@@ -48,7 +48,7 @@ const ServiceSection = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col justify-center items-center pt-[7em] mx-[5em] w-[20%]">
+                <div className="flex flex-col justify-center items-center lg:pt-[7em] lg:mx-[2em] sm:mx-[3em] lg:w-[25%] md:w-[37%] sm:w-[50%] w-[90%]">
                     <img src={CardImg1} className='h-[25em] w-[21em] border-[10px] border-[white] shadow-2xl' alt="" />
                     <div className="flex flex-col justify-center items-start w-[88%] mt-[40px]">
                         <h5 className='text-[2em] font-secondary font-semibold drop-shadow-xl'>Card Title</h5>
@@ -57,7 +57,7 @@ const ServiceSection = () => {
                     </div>
                 </div>
 
-                <div className="flex flex-col justify-center items-center mx-[5em] w-[20%]">
+                <div className="flex flex-col justify-center items-center lg:mx-[2em] sm:mx-[3em] lg:w-[25%] md:w-[37%] sm:w-[50%] w-[90%]">
                     <img src={CardImg1} className='h-[25em] w-[21em] border-[10px] border-[white] shadow-2xl' alt="" />
                     <div className="flex flex-col justify-center items-start w-[88%] mt-[40px]">
                         <h5 className='text-[2em] font-secondary font-semibold drop-shadow-xl'>Card Title</h5>
