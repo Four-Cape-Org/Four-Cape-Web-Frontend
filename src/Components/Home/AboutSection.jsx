0a560d9cf6bf8c891aa0a077d9eddd63aeadd35f
muteenk <kundangarmuteen@gmail.com>
2024-02-11 15:55:09 +0530

    Ad section in about section made responsive

--- src/Components/Home/AboutSection.jsx
+++ src/Components/Home/AboutSection.jsx
@@ -22,10 +22,10 @@ const AboutSection = (props) => {
         </div>
       </div>
 
-      <div className='relative mt-[4em]'>
-        <h2 className='font-secondary text-[4em] drop-shadow-xl text-center underline'>Want to know more ? Check this out.</h2>
+      <div className='relative lg:mt-[3em] md:mt-[4em] sm:mt-[5em] mt-[7em]'>
+        <h2 className='font-secondary lg:text-[4em] md:text-[3em] sm:text-[2em] text-[2em] mx-4 drop-shadow-xl text-center underline'>Want to know more ? Check this out.</h2>
         <div className='relative flex justify-center items-center my-[3em] cursor-pointer'>
-          <img src={AdThumbnail} className='h-[40em] rounded-md shadow-[35px_35px_10px_-15px_#F82586]' alt="Advertisment Thumbnail" />
+          <img src={AdThumbnail} className='lg:w-[80%] md:w-[80%] sm:w-[90%] w-[95%] rounded-md shadow-[35px_35px_10px_-15px_#F82586]' alt="Advertisment Thumbnail" />
           <button className='absolute top-[45%] rounded-[50%] bg-accent-color border-[2px] border-accent-color hover:border-[white] h-[2.5em] w-[2.5em] text-[50px]'><i className="fa-solid fa-play"></i></button>
         </div>
       </div>
