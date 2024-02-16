5d11e40c20bbc1502100cc2b3672806a918b1dd2
muteenk <kundangarmuteen@gmail.com>
2024-02-16 17:01:06 +0530

    Portfolio Heading made responsive

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -18,9 +18,9 @@ const Portfolio = () => {
   return (
     <>
         <Navbar bookCallRef={bookCallRef}/>
-        <div className='relative h-[22em] w-screen mt-5'>
+        <div className='relative lg:h-[22em] md:h-[14em] sm:h-[11em] h-[7em] w-screen mt-5'>
           <OverlayText className="absolute top-0 left-10 z-[0] text-[18vw] max-[1250px]:text-[16vw] max-[550px]:left-3" text="Our Work" />
-          <h2 className='absolute top-[70%] left-[50%] translate-x-[-50%] text-accent-color font-bold font-primary text-[15px] sm:text-[15px] md:text-[20px] lg:text-[45px]'>Porfolio</h2>
+          <h2 className='absolute top-[70%] left-[50%] translate-x-[-50%] text-accent-color font-bold font-primary text-[22px] sm:text-[27px] md:text-[35px] lg:text-[45px]'>Porfolio</h2>
         </div>
         <ShowReel/>
         <Reels/>
