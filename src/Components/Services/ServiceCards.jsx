bb92499868305f9a5f7f33280f8a3b6f3e83b467
muteenk <kundangarmuteen@gmail.com>
2024-08-28 01:19:18 +0530

    Initiated service cards

--- src/Components/Services/ServiceCards.jsx
+++ src/Components/Services/ServiceCards.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ServiceData from './ServiceCardsData.js'
+import BubbleButtonPrimary from '../Elements/BubbleButtonPrimary'
 
 /*
  * ServiceCard components
@@ -14,13 +15,27 @@ import ServiceData from './ServiceCardsData.js'
 
 const ServiceCard = (props) => {
   return (
-      <div className="flex w-[90%] bg-[#f2f2f2] rounded-[20px] mx-auto">
-        <div className="w-full h-full bg-accent-color rounded-t-[20px]">
+      <div className="flex w-[90%] rounded-[20px] mx-auto">
+        <div className="h-full bg-accent-color rounded-t-[20px]">
           <img src={props.image} alt={props.altText} className="w-full h-full object-cover rounded-t-[20px]" />
         </div>
         <div className="top-[50px] left-0 w-full p-[20px]">
           <h3 className="font-primary text-[20px] font-bold text-accent-color">{props.title}</h3>
           <p className="font-primary text-[15px] text-[#6e6e6e] mt-[10px]">{props.description}</p>
+          {
+            (props.offers.length > 0) ? (
+              <ul className="font-primary text-[15px] text-[#6e6e6e] mt-[10px]">
+                {
+                  props.offers.map((offer, index) => {
+                    return (
+                      <li key={index}>{offer}</li>
+                    )
+                  })
+                }
+              </ul>
+            ) : null
+          }
+          <BubbleButtonPrimary sendTo="/contact" text="Lets Talk" />
         </div>
       </div>
   )
