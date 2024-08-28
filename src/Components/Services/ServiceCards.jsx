faafcc427a4fe80b5c47dd5da44ac79350947daa
muteenk <kundangarmuteen@gmail.com>
2024-08-29 01:32:24 +0530

    Updated Services Cards

--- src/Components/Services/ServiceCards.jsx
+++ src/Components/Services/ServiceCards.jsx
@@ -13,29 +13,42 @@ import BubbleButtonPrimary from '../Elements/BubbleButtonPrimary'
  *  
  * */
 
+function TextWithLineBreaks(props) {
+  const textWithBreaks = props.text.split('\n').map((text, index) => (
+    <React.Fragment key={index}>
+      {text}
+      <br />
+    </React.Fragment>
+  ));
+
+  return <div>{textWithBreaks}</div>;
+}
+
 const ServiceCard = (props) => {
   return (
-      <div className="flex w-[90%] rounded-[20px] mx-auto">
-        <div className="h-full bg-accent-color rounded-t-[20px]">
-          <img src={props.image} alt={props.altText} className="w-full h-full object-cover rounded-t-[20px]" />
+      <div className={"flex justify-start items-start w-[90%] rounded-[20px] my-[2em]"+props.className}>
+        <div className="flex justify-center items-center h-full rounded-[20px]">
+          <img src={props.image} alt={props.altText} className="w-[30em] object-cover rounded-[20px]" />
         </div>
-        <div className="top-[50px] left-0 w-full p-[20px]">
-          <h3 className="font-primary text-[20px] font-bold text-accent-color">{props.title}</h3>
-          <p className="font-primary text-[15px] text-[#6e6e6e] mt-[10px]">{props.description}</p>
+        <div className="w-full px-[18px]">
+          <h2 className="font-primary text-[25px] font-bold text-accent-color">{props.title}</h2>
+          <p className="font-primary text-[15px] mt-[10px]"><TextWithLineBreaks text={props.description} /></p>
           {
             (props.offers.length > 0) ? (
-              <ul className="font-primary text-[15px] text-[#6e6e6e] mt-[10px]">
+              <ul className="font-primary text-[15px] mt-[2em] ml-[4em]">
                 {
                   props.offers.map((offer, index) => {
                     return (
-                      <li key={index}>{offer}</li>
+                      <li key={index}>✨ {offer}</li>
                     )
                   })
                 }
               </ul>
             ) : null
           }
-          <BubbleButtonPrimary sendTo="/contact" text="Lets Talk" />
+          <div className="flex justify-end mt-[20px]">
+            <BubbleButtonPrimary sendTo="/contact" text="Lets Talk" />
+          </div>
         </div>
       </div>
   )
@@ -43,11 +56,11 @@ const ServiceCard = (props) => {
 
 const ServiceCards = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-screen">
+    <div className="flex flex-col mx-[2em] w-screen">
       {
         ServiceData.map((service) => {
           return (
-            <ServiceCard key={service.id} title={service.title} description={service.description} image={service.image} altText={service.altText} offers={service.offers} />
+            <ServiceCard key={service.id} className="even:flex-row-reverse" title={service.title} description={service.description} image={service.image} altText={service.altText} offers={service.offers} />
           )
         })
       }
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -17,7 +17,7 @@ const Services = () => {
       <OverlayText className="absolute top-0 left-10 z-[0] text-[20vw] max-[1250px]:text-[18vw] max-[550px]:left-3" text="Services" />
         <div className='relative pt-[19em] max-[950px]:pt-[10em] max-[550px]:pt-[5em] pb-[3em]'>
           <h4 className='text-accent-color font-bold font-primary text-[20px] my-[10px] text-center'>Services</h4>
-          <p className='font-primary text-[15px] w-[45%] my-[20px] mx-auto text-center max-[950px]:w-[70%] max-[550px]:w-[90%] max-[550px]:text-[12px]'>
+          <p className='font-primary text-[15px] w-[45%] my-[20px] mx-auto text-center max-[950px]:w-[70%] max-[550px]:w-[90%] max-[550px]:text-[12px] text-[#6e6e6e]'>
             <b>Fourcape</b> is a full-service brand-building agency dedicated to driving business success through creative and strategic solutions. We believe in the power of well-crafted brands to transform markets and empower businesses to achieve their full potential. Our approach is rooted in our core philosophy, represented by <b> CAPE: Creating Path, Adapting Market, Promoting Brands, Empowering Businesses </b>.
 
 With a focus on design excellence, market insight, and brand strategy, our team works collaboratively to deliver innovative solutions tailored to the unique needs of each client. Whether it’s brand development, market positioning, or visual identity, Fourcape is committed to guiding your business on the path to success in an ever-evolving marketplace.
